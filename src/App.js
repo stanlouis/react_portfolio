bdfb90394803f2247720d43f7ee7f209ea3ce230
Stanley Louis <stanlouis@msn.com>
2019-01-28 06:43:57 -0500

    Add Projects component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Projects from "./Projects";
 
 class App extends Component {
+  state = {
+    displayBio: false
+  };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Stanley. I'm a software engineer.</p>
+        <p>
+          My favorite language is JavaScript and my favorite front-end framework
+          is React.
+        </p>
+        <p>I'm always looking forward to working on meaningful projects.</p>
+        <button onClick={this.toggleDisplayBio}>Show less</button>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Maryland, and code every day.</p>
+            <p>Besides coding, I also love hiking and diving</p>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read more</button>
+          </div>
+        )}
+        <hr />
+        <Projects />
       </div>
     );
   }
---
+++ src/data/projects.js
@@ -0,0 +1,27 @@
+import project1 from "../assets/trucktrack.png";
+import project2 from "../assets/indecision.png";
+import project3 from "../assets/zero-hour.png";
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: "TruckTrack",
+    description:
+      "TruckTrack helps connect food truck owners and their customers.",
+    image: project1
+  },
+  {
+    id: 2,
+    title: "Indecision",
+    description: "A Task ap that randomly select A task from a list",
+    image: project2
+  },
+  {
+    id: 3,
+    title: "Zero Hour",
+    description: "A Countdown Timer.",
+    image: project3
+  }
+];
+
+export default PROJECTS;
