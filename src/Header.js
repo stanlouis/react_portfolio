992172f9f10e5c65df4db3d6523469337850bd92
Stanley Louis <stanlouis@msn.com>
2019-01-29 14:16:23 -0500

    Header styles tweak

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = ({ children }) => (
   <React.Fragment>
-    <nav className="navbar navbar-expand-lg navbar-light  bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark mb-5 bg-dark">
       <Link className="navbar-brand" to="/">
         Home
       </Link>
